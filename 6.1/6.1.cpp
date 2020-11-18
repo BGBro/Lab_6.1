67e65364d1a33c2b0efc901144cb7f125a98c2ec
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2020-11-18 11:55:48 +0200

    master

---
+++ 6.1/6.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <sstream>
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    istringstream input1("bogdan den mon vitalik nesty");
+    vector <string> penis;
+    string s;
+    while (input1 >> s)
+        penis.push_back(s);
+
+    while (penis.size() != 0) {
+        int a = rand() % penis.size();
+        cout << penis[a] << endl;
+        penis.erase(penis.begin() + a);
+    }
+    return 0;
+}
